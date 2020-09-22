c5eb552c472b537cd87e7cafd0a66bceac175a4c
designervoid <newluciocheg@gmail.com>
2020-09-22 17:51:31 +0300

    добавил модули vuex

---
+++ store/country.js
@@ -0,0 +1,17 @@
+const state = () => ({});
+
+const getters = {};
+
+const actions = {};
+
+const mutations = {};
+
+const modules = {};
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations,
+  modules,
+};
--- store/user.js
+++ store/user.js
@@ -1,2 +1,27 @@
-export const state = () => ({})
-export const mutations = {}
+const state = () => ({
+    userDetails: {
+      name: '',
+      lastName: ''
+    }
+});
+
+const getters = {};
+
+const actions = {};
+
+const mutations = {
+  SET_USER_DETAILS(state, payload) {
+    state.userDetails = payload
+  },
+};
+
+const modules = {};
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations,
+  modules,
+};
